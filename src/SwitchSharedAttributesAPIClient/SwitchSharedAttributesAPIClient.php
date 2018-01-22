1942b947f37d358cf49b6b382bc17f164864170b
Lionel Walter <lionel.walter@unibas.ch>
2018-01-22 16:10:04 +0100

    init travis

---
+++ src/SwitchSharedAttributesAPIClient/SwitchSharedAttributesAPIClient.php
@@ -0,0 +1,43 @@
+<?php
+/**
+ * Client for SWITCH Edu-ID Shared Attributes API
+ *
+ * PHP version 5
+ * Copyright (C) project swissbib, University Library Basel, Switzerland
+ * http://www.swissbib.org  / http://www.swissbib.ch / http://www.ub.unibas.ch
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License version 2,
+ * as published by the Free Software Foundation.
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
+ *
+ * @author   Lionel Walter <lionel.walter@unibas.ch>
+ * @license  http://opensource.org/licenses/gpl-2.0.php GNU General Public License
+ * @link     http://www.swissbib.ch
+ */
+namespace SwitchSharedAttributesAPIClient;
+
+/**
+ * Class SwitchSharedAttributesAPIClient
+ *
+ * @author  Lionel Walter <lionel.walter@unibas.ch>
+ * @license http://opensource.org/licenses/gpl-2.0.php GNU General Public License
+ * @link    http://www.swissbib.ch
+ */
+class SwitchSharedAttributesAPIClient
+{
+    /**
+     * Small test
+     *
+     * @return void
+     */
+    public function helloWorld()
+    {
+        echo("Hello World !");
+    }
+}
